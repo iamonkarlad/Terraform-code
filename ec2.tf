7c0d0d3c15772e3267a8d3564d1d8b4b499d11d5
iamonkarlad <ladonkar9@gmail.com>
2025-08-23 20:34:22 +0530

    addd

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "my_aws_instance" {
+    ami = "ami-00ca32bbc84273381"
+    instance_type = "t2.micro"
+    tags = {
+    Name = "instance "
+    }
+}
